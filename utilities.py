dd4c738c1325ab73e4be9c11fcdebb7428e74975
Sergey Pavlov <qiwichupa@gmail.com>
2019-02-17 16:17:54 +0000

    Replace utilities.py

--- utilities.py
+++ utilities.py
@@ -12,7 +12,9 @@ def bool2str(arg):
 
 
 
-
+def mysql_query_wildficator(query):
+    wild_query = query.replace("%", "\%").replace("_", "\_").replace("*", "%").replace("?", "_")
+    return(wild_query)
 
 def scantree(path):
     """Recursively yield DirEntry objects for given directory.
@@ -24,12 +26,34 @@ def scantree(path):
     for entry in scandir(path):
         if entry.is_dir(follow_symlinks=False):
             try:
-                yield entry
+                #yield entry
                 yield from scantree(entry.path)
             except Exception as e:
-                print("Scantree func error(dir): " + str(e))
-        else:
+                print(str(e))
+        elif entry.is_file():
             try:
                 yield entry
             except Exception as e:
-                print("Scantree func error(file): " + str(e))
+                print(str(e))
+        else:
+            pass
+
+class LoggerWriter:
+    def __init__(self, level):
+        # self.level is really like using log.debug(message)
+        # at least in my case
+        self.level = level
+
+    def write(self, message):
+        # if statement reduces the amount of newlines that are
+        # printed to the logger
+        if message != '\n':
+            self.level(message)
+
+    def flush(self):
+        # create a flush method so things can be flushed when
+        # the system wants to. Not sure if simply 'printing'
+        # sys.stderr is the correct way to do it, but it seemed
+        # to work properly for me.
+        #self.level(sys.stderr)
+        pass
